7c98288eed73e440344492fa1cb6c92e3ad91501
SoulixTech <pranavnavghare46@gmail.com>
2025-11-05 20:43:20 +0530

    Add project files and remove large .exe

--- watch.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Starting Tailwind CSS in watch mode...
-echo Changes to HTML/CSS will automatically rebuild.
-echo Press Ctrl+C to stop.
-echo.
-tailwindcss.exe -i src/input.css -o dist/output.css --watch
